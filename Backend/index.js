0f9ba2029f1a6a0de63096b21f1ab85a82fc3a39
ElioFezollari <fezollarielio@gmail.com>
2025-04-06 18:07:23 -0400

    Changed Backend For Prod

--- Backend/index.js
+++ Backend/index.js
@@ -1,44 +1,42 @@
 var express = require('express');
-var { graphqlHTTP }  = require('express-graphql');
+var { graphqlHTTP } = require('express-graphql');
 const { default: mongoose } = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
-const {userSchema} = require('./schema/user')
-const {employeeSchema} = require('./schema/employee')
+
+const { userSchema } = require('./schema/user');
+const { employeeSchema } = require('./schema/employee');
+
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
+  useNewUrlParser: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
   })
-    .then(() => {
-      console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-      console.error('MongoDB connection error:', err);
-    });
-  
-
-
-
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 
 let app = express();
 app.use(cors());
 
 app.use('/graphql/user', graphqlHTTP((req, res, graphQLParams) => {
-  console.log('Incoming GraphQL Request:');
-  
-  if (graphQLParams) {
-    console.log('Query:', graphQLParams.query);
-    console.log('Variables:', graphQLParams.variables);
-  }
-
   return {
     schema: userSchema,
     graphiql: true,
   };
 }));
 
-  app.use('/graphql/employee', graphqlHTTP({
-    schema: employeeSchema,
-    graphiql: true, 
-  }));
+app.use('/graphql/employee', graphqlHTTP({
+  schema: employeeSchema,
+  graphiql: true,
+}));
+app.use(express.static(path.join(__dirname, 'dist/101410182_comp3133_assignment2/browser')));
 
-app.listen(4000, () => console.log('Express GraphQL Server Now Running On http://localhost:4000/graphql'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/101410182_comp3133_assignment2/browser', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
